17a6f5da85aea6725b5f7654088d21cd4a66878d
Joona Lusa <joona.lusa@hotmail.com>
2021-12-17 17:36:47 -0500

    user is able to generate a readme.md file with all appropriate sections

--- README.md
+++ README.md
@@ -1,16 +1,39 @@
-# Professional README.md Generator 
+# Professional README.md Generator
+ 
+[![license](https://img.shields.io/badge/license-ISC License-blue)](https://shields.io)
+  
+## Description 
 
-## Description
-This project contains an application that can be used to generate a README.md file. 
+This application can be used to generate a README.md file. 
 
 ## Table of Contents 
 
 * [Installation](#installation)
 * [Usage](#usage)
-* [Credits](#credits)
+* [Contribution Guidelines](#contribution)
 * [License](#license) 
 
-## Questions
+## Installation
+Please ensure to have Inquirer installed on your machine as well as JSON packages.
+      
+
+## Usage
+NADA
+  
+
+## Contribution
+NADA
+
+
+## Test
+
+## License 
+
+_This application has the ISC License._
+For more information, [license]()
+    
+## Questions 
+
 Please do not hesitate to contact me with further questions!
-GitHub: [jlusa101](https://github.com/jlusa101) 
-Email: joona.lusa@hotmail.com
+* GitHub: [jlusa101](https://github.com/jlusa101) 
+* Email: joona.lusa@hotmail.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,30 +1,89 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
+function renderLicenseBadge(data) {
+    return ` 
+[![license](https://img.shields.io/badge/license-${data.license}-blue)](https://shields.io)
+  `;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+let licenseLink = '';
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+
+    if (data.license === "MIT") {
+        licenseLink = 'https://choosealicense.com/licenses/mit/';
+    } else if (data.license === "Apache License 2.0") {
+        licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
+    } else if (data.license === "GNU GPLv3") {
+        licenseLink = 'https://choosealicense.com/licenses/gpl-3.0/';
+    } else if (data.license === "ISC License") {
+        licenseLink = 'https://choosealicense.com/licenses/isc/';
+    }
+    return `## License \n
+_This application has the ${data.license}._
+For more information, [license](${licenseLink})
+    `;
+}
+
+function renderInstallationInstructions(data) {
+    if (data.confirmInstallation) {
+        return `## Installation
+${data.installation}
+      `;
+    } else {
+        return `## Installation`;
+    }
+}
+
+function renderUsageInformation(data) {
+    if (data.confirmUsage) {
+        return `## Usage
+${data.usage}
+  `;
+    } else {
+        return `## Usage`;
+    }
+}
+
+function renderContribution(data) {
+    if (data.confirmContribution) {
+        return `## Contribution
+${data.contribution}
+`;
+    } else {
+        return `## Contribution`;
+    }
+}
+
+function renderTest(data) {
+    if (data.confirmTest) {
+        return `## Test
+${data.test}
+`;
+    } else {
+        return `## Test`;
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} \n
-## Description
+
+    return `# ${data.title}
+${renderLicenseBadge(data)}
+## Description \n
 ${data.description} \n
 ## Table of Contents \n
 * [Installation](#installation)
 * [Usage](#usage)
-* [Credits](#credits)
+* [Contribution Guidelines](#contribution)
 * [License](#license) \n
-## Questions
+${renderInstallationInstructions(data)}\n
+${renderUsageInformation(data)}\n
+${renderContribution(data)}\n
+${renderTest(data)}\n
+${renderLicenseSection(data)}
+## Questions \n
 Please do not hesitate to contact me with further questions!
-GitHub: [${data.username}](https://github.com/jlusa101) 
-Email: ${data.email}
+* GitHub: [${data.username}](https://github.com/jlusa101) 
+* Email: ${data.email}
 `;
 }
 
